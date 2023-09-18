26b9170c3e5abef7c3be65ef512a10eaec2d4789
heyitslauu <laurencecadag1@gmail.com>
2023-09-18 15:37:04 +0800

    Live Display prop-based value

--- src/components/DisplayHeader.jsx
+++ src/components/DisplayHeader.jsx
@@ -1,14 +1,15 @@
+/* eslint-disable react/prop-types */
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { Pin, Email, Phone } from "../styles/fontAwesome";
-function DisplayHeader(){
+function DisplayHeader({formInput}){
     return (
         <div className="min-h-[10rem] bg-blue-900 text-white flex flex-col items-center justify-center">
-            <h1 className="cv-owner text-3xl font-bold mb-4">John Doe</h1>
+            <h1 className="cv-owner text-3xl font-bold mb-4">{formInput.fullName}</h1>
             <div className="flex gap-5">
-                <p className="flex gap-3 items-center text-lg"><FontAwesomeIcon icon={Email} />johndoe@email.xyz</p>
-                <p className="flex gap-3 items-center text-lg"><FontAwesomeIcon icon={Phone} />09123456789</p>
-                <p className="flex gap-3 items-center text-lg"><FontAwesomeIcon icon={Pin} />New York City</p>
+                <p className="flex gap-3 items-center text-lg"><FontAwesomeIcon icon={Email} />{formInput.email}</p>
+                <p className="flex gap-3 items-center text-lg"><FontAwesomeIcon icon={Phone} />{formInput.phone}</p>
+                <p className="flex gap-3 items-center text-lg"><FontAwesomeIcon icon={Pin} />{formInput.address}</p>
             </div>
         </div>
     )
