282573d384e24dd000098f1ec431e1c280a8d70a
heyitslauu <laurencecadag1@gmail.com>
2023-09-19 16:50:30 +0800

    Dynamic Display Experience

--- src/components/DisplayExperience.jsx
+++ src/components/DisplayExperience.jsx
@@ -1,30 +1,24 @@
-function DisplayExperience() {
+/* eslint-disable react/prop-types */
+function DisplayExperience({experiences}) {
     return (
         <div className="w-4/5 mx-auto">
             <h1 className="bg-gray-200 py-2 px-2 font-black text-center uppercase text-xl mb-2">Experience</h1>
-            <div className="grid grid-cols-[1fr_2fr] w-full ">
-                <div className="expDuration">4/2023 - Present</div>
-                <div className="expDescription">
-                    <div>
-                        <h2 className="expCompany font-bold text-lg">John Doe Consulting</h2>
-                        <h3 className="expPosition">Front End Engineer</h3>
-                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At explicabo pariatur facilis tempore!</li>   
-                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At explicabo pariatur facilis tempore!</li>   
+            {experiences.map((expi) => {
+                return (
+                    <div key={expi.id}>
+                        <div  className="grid grid-cols-[1fr_2fr] w-full">
+                            <div className="expDuration">{expi.startDate} - {expi.endDate}</div>
+                            <div className="expDescription">
+                                <div>
+                                    <h2 className="expCompany font-bold text-xl">{expi.companyName}</h2>
+                                    <h3 className="expPosition text-gray-700 text-lg">{expi.companyPosition}</h3>
+                                    <p>{expi.description}</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div className="grid grid-cols-[1fr_2fr] w-full ">
-                <div className="expDuration">4/2022 - 6/2023</div>
-                <div className="expDescription">
-                    <div>
-                        <h2 className="expCompany font-bold text-lg">Doe John Consulting</h2>
-                        <h3 className="expPosition">Full Stack Engineer</h3>
-                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At explicabo pariatur facilis tempore!</li>   
-                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At explicabo pariatur facilis tempore!</li>   
-                    </div>
-                </div>
-            </div>
-        
+                )
+            })}  
         </div>
     )
 }
