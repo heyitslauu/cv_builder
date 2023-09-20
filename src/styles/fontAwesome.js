2dcfedd19580e9ad5de02eaa5d6d773b725b15a6
heyitslauu <laurencecadag1@gmail.com>
2023-09-20 21:23:46 +0800

    font awesome font icons

---
+++ src/styles/fontAwesome.js
@@ -0,0 +1,31 @@
+import { 
+    faAngleDown, 
+    faAngleUp, 
+    faBriefcase, 
+    faPlus, 
+    faUser,
+    faWrench,
+    faGraduationCap,
+    faPhone,
+    faLocationDot,
+    faClose,
+    faPenToSquare,
+    faSquareMinus
+} from "@fortawesome/free-solid-svg-icons";
+
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+
+export const CaretDown = faAngleDown;
+export const CaretUp = faAngleUp;
+export const BriefCase = faBriefcase;
+export const Plus = faPlus;
+export const User = faUser;
+export const Tools = faWrench;
+export const GradCap = faGraduationCap;
+export const Email = faEnvelope;
+export const Phone = faPhone;
+export const Pin = faLocationDot;
+export const Close = faClose;
+export const Edit = faPenToSquare;
+export const Remove = faSquareMinus
