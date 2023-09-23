407618aeeb38138ed276a4be53d0ca9fb5934fa3
heyitslauu <laurencecadag1@gmail.com>
2023-09-23 23:29:09 +0800

    Dynamic Display education

--- src/components/DisplayEducation.jsx
+++ src/components/DisplayEducation.jsx
@@ -1,16 +1,25 @@
-function DisplayEducation() {
+/* eslint-disable react/prop-types */
+function DisplayEducation({education}) {
     return(
         <div className="w-4/5 mx-auto">
             <h1 className="bg-gray-200 py-2 px-2 font-black text-center uppercase text-xl mb-2">Education</h1>
-            <div className="grid grid-cols-[1fr_2fr] w-full ">
-                <div className="educDuration">4/2017 - 4/2021</div>
-                <div className="educDescription">
-                    <div>
-                        <h2 className="font-bold text-lg">Yale University</h2>
-                        <h3>Bachelor&apos;s in Computer Science</h3>
+            {education.map((educ) => {
+                return (
+                    <div key={educ.id}>
+                        <div className="grid grid-cols-[1fr_2fr] w-full ">
+                            <div className="educDuration">{educ.completionDate}</div>
+                            <div className="educDescription">
+                                <div>
+                                    <h2 className="font-bold text-lg">{educ.school}</h2>
+                                    <h3>{educ.degree}</h3>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
+                )
+            })
+
+            }
         </div>
     )
 }
