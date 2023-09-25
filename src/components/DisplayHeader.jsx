2b6ef87acc7a262ff5cf807b83c67d7de49bac67
heyitslauu <laurencecadag1@gmail.com>
2023-09-25 22:00:02 +0800

    Flex wrap

--- src/components/DisplayHeader.jsx
+++ src/components/DisplayHeader.jsx
@@ -6,7 +6,7 @@ function DisplayHeader({formInput}){
     return (
         <div className="min-h-[10rem] bg-blue-900 text-white flex flex-col items-center justify-center">
             <h1 className="cv-owner text-3xl font-bold mb-4">{formInput.fullName}</h1>
-            <div className="flex gap-5">
+            <div className="flex gap-5 flex-wrap items-center justify-center">
                 <p className="flex gap-3 items-center text-lg"><FontAwesomeIcon icon={Email} />{formInput.email}</p>
                 <p className="flex gap-3 items-center text-lg"><FontAwesomeIcon icon={Phone} />{formInput.phone}</p>
                 <p className="flex gap-3 items-center text-lg"><FontAwesomeIcon icon={Pin} />{formInput.address}</p>
